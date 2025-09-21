32077294ebb7669e512791126581204fdbee46af
daniellvaz <daniellmvaz@gmail.com>
2025-09-20 23:11:24 -0300

    last commit

--- api_fornecedores/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FornecedorController;
-
-Route::get('/fornecedores', [FornecedorController::class, 'index']);
-Route::post('/fornecedores', [FornecedorController::class, 'store']);
